f4a487b900ea12d4b658fd64fa7f38ebda31be9f
AndruXxX <saldj.andre@gmail.com>
2023-04-09 15:03:06 +0300

    init commit

---
+++ src/main/java/com/project/car_dealership_service/CarDealershipServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.car_dealership_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealershipServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarDealershipServiceApplication.class, args);
+    }
+
+}
