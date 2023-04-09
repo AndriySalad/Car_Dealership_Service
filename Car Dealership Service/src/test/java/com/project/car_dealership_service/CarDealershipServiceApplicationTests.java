bac10d1e21fdba80a92d9da4a925b65ddc62c1a2
AndruXxX <74617617+AndruXxX@users.noreply.github.com>
2023-04-09 15:07:34 +0300

    init commit

---
+++ Car Dealership Service/src/test/java/com/project/car_dealership_service/CarDealershipServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.car_dealership_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDealershipServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
